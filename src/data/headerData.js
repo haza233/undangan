cc512bf6bcd1eb70c383e5bc8de5d8112a121822
haza305 <rifqiajja55@gmail.com>
2024-09-27 16:55:14 +0700

    new

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+export const headerData = {
+  name: "Muhmmad Rifqi",
+  title: "Hello! I am",
+  desciption:
+    "Hello! I am Muhammad Rifqi, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+  image: "https://i.ibb.co/LSrpds8/rupa1-21-1.jpg",
+  // imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+  resumePdf:
+    "https://drive.google.com/file/d/14EO5OOPB3mzUB2DoVSVjtfchzHCNXAlT/view?usp=sharing",
+};
---
+++ src/data/projectsData.js
@@ -0,0 +1,49 @@
+import eight from "../assets/svg/projects/eight.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import six from "../assets/svg/projects/six.svg";
+
+export const projectsData = [
+  {
+    id: 1,
+    projectName: "Dasboard Test Drive Telkomsel",
+    projectDesc: "This project is about Telkomsel dashboard test drive.",
+    tags: ["React", "React Bootstrap"],
+    code: "",
+    demo: "https://dashboarddttsel.com/",
+    image: six,
+  },
+  {
+    id: 2,
+    projectName: "Website Absen karyawan picotel",
+    projectDesc:
+      "Where I provide support for absenteeism repairs and adding requests from user's using Laravel",
+    tags: ["Laravel Lumen", "Material Ui"],
+    code: "",
+    demo: "https://picosd.com/",
+    image: seven,
+  },
+  {
+    id: 3,
+    projectName: "Website Biro Jasa Global Jaya sejahtera",
+    projectDesc: "This project about travel agency management.",
+    tags: ["React", "CSS", "Material Ui"],
+    code: "",
+    demo: "https://globaljayasejahtera.com/",
+    image: eight,
+  },
+];
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
